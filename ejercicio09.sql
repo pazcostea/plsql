08e413988bb1c5886a67f5bc3b90a4259635e872
pazcostea <pazcostea@gmail.com>
2019-07-16 14:58:49 +0200

    Total compras usuario Terminado

--- ejercicio09.sql
+++ ejercicio09.sql
@@ -1,27 +1,30 @@
 --Numero de compras por usuario
+--Total gastado por usuario
 DECLARE     
     CURSOR pedidos IS SELECT * FROM DEMO_CUSTOMERS;
-    CURSOR pedidos_usuario (identUsu NUMBER) IS SELECT * FROM DEMO_ORDERS WHERE CUSTOMER_ID = identUsu;
+    CURSOR pedidos_usuario (identUsu NUMBER) 
+    IS SELECT DEMO_ORDERS.CUSTOMER_ID, SUM(DEMO_ORDERS.ORDER_TOTAL) AS total_compra 
+    FROM DEMO_ORDERS 
+    WHERE CUSTOMER_ID = identUsu 
+    GROUP BY DEMO_ORDERS.CUSTOMER_ID;
     pedid_usu pedidos_usuario%ROWTYPE;
     pedid pedidos%ROWTYPE;
     total_pedidos NUMBER (3) := 0;
-    total_compra NUMBER(3) := 0;
+    total_compra NUMBER(8) := 0;
 BEGIN
     OPEN pedidos;
-        LOOP            
+        LOOP
             FETCH pedidos INTO pedid;            
             EXIT WHEN pedidos%NOTFOUND;
-            SELECT count(*) INTO total_pedidos FROM DEMO_ORDERS WHERE CUSTOMER_ID = pedid.CUSTOMER_ID;
-            OPEN pedidos_usuario (pedid.CUSTOMER_ID);
-                FETCH pedidos_usuario INTO pedid_usu;
-                DBMS_OUTPUT.PUT_LINE('Total DENTRO: ' ||pedid_usu.ORDER_TOTAL);  
-                EXIT WHEN pedidos_usuario%NOTFOUND;                
-            CLOSE pedidos_usuario;
-            DBMS_OUTPUT.PUT_LINE('ID usuario: ' ||pedid.CUSTOMER_ID);
-            DBMS_OUTPUT.PUT_LINE('Total compras: ' ||total_pedidos);
-            DBMS_OUTPUT.PUT_LINE('Total: ' ||pedid_usu.ORDER_TOTAL);            
-        END LOOP;
-        
+            SELECT count(*) INTO total_pedidos FROM DEMO_ORDERS WHERE CUSTOMER_ID = pedid.CUSTOMER_ID;            
+            OPEN pedidos_usuario (pedid.CUSTOMER_ID);               
+                LOOP                
+                FETCH pedidos_usuario INTO pedid_usu;                    
+                EXIT WHEN pedidos_usuario%NOTFOUND;
+                END LOOP;
+                DBMS_OUTPUT.PUT_LINE('ID usuario: ' ||pedid.CUSTOMER_ID || ' total compras: ' ||total_pedidos || ' total ' || pedid_usu.total_compra);
+            CLOSE pedidos_usuario;            
+        END LOOP;        
     CLOSE pedidos;
     
     
