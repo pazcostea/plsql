8ec7cb26fac86b40780e3a8254f3f5bddb481833
pazcostea <pazcostea@gmail.com>
2019-07-17 13:15:24 +0200

    Clientes y factorial

--- ejercicio11.sql
+++ ejercicio11.sql
@@ -1,11 +1,16 @@
 --Subprograma que muestre el nombre del mejor cliente
-CREATE OR REPLACE PROCEDURE nombre_cliente (id_cli number) IS
-    --nombre VARCHAR2;
-BEGIN    
-    --nombre := 'Pepe';
-    DBMS_OUTPUT.PUT_LINE(id_cli);
+CREATE OR REPLACE PROCEDURE nombre_cliente IS
+    nombre VARCHAR2 (100);
+    id_cliente NUMBER(10);
+BEGIN
+    id_cliente := mejorcliente;
+    DBMS_OUTPUT.PUT_LINE(id_cliente);    
+    SELECT DEMO_CUSTOMERS.CUST_FIRST_NAME INTO nombre
+        FROM DEMO_CUSTOMERS
+        WHERE DEMO_CUSTOMERS.CUSTOMER_ID = id_cliente;
+    DBMS_OUTPUT.PUT_LINE(nombre);
 END;
 /
 BEGIN
-    nombre_cliente(1);
+    nombre_cliente;
 END;
